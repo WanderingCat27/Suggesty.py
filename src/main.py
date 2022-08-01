a99a7a2e87e10911d1975857512c90fb23324fe7
WanderingCat27 <51253586+WanderingCat27@users.noreply.github.com>
2022-07-31 22:14:13 -0400

    active build of bot uses json not .env

--- src/main.py
+++ src/main.py
@@ -1,14 +1,12 @@
 import nextcord
 from nextcord.ext import commands
 from os import getenv
-from dotenv import load_dotenv
 import d_bot as bot
+import json
 
 from slash_commands.commands import register_commands
 from utils.json_utils import get_suggestion_channel, get_watching
 from utils.suggestions import create_suggestion
-
-
 
 
 @bot.BOT.event
@@ -20,10 +18,8 @@ async def on_message(message : nextcord.Message):
     if get_watching() and message.channel == get_suggestion_channel() and message.author.id != bot.BOT.user.id and message.type == nextcord.MessageType.default:
         await create_suggestion(message.content, message.author)
         await message.delete()
-
-load_dotenv()
 register_commands(bot.BOT)
 
 
-
-bot.BOT.run(getenv("TOKEN"))
+with open("cred.json") as f:
+	bot.BOT.run(json.load(f)["TOKEN"])
